d4ec64740ff71a6bafde8cccfa56131a8a836016
facundoaquino <facun90@live.com>
2021-02-15 19:09:07 -0300

    finish register page started registering method

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,24 @@
-
-
 /*---------------------- database required ---------------------*/
 
-const db = require('../db/models')
+const db = require("../db/models");
+
+/*---------------------- hashing password ---------------------*/
+
+const bcrypt = require('bcryptjs')
 
 
 const userController = {
-	register:  (req, res) => {
+  register: (req, res) => {
+    res.render("register");
+  },
 
+  registering: async (req, res) => {
 
-		res.render('register');
-	}
+	const passwordHashed = bcrypt.hashSync(req.body.password,10)
+	const userEmail = req.body.email
+
+	await db.User.create({email:userEmail,password:passwordHashed})
+  },
 };
 
 module.exports = userController;
