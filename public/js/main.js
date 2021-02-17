32488c961f8c43ecde481c09c55c819fbf3d5d8f
facundoaquino <facun90@live.com>
2021-02-16 23:32:29 -0300

    carousel whit vanilla js and zoom images by lightbox

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+
+/*---------------------- carrousel banner ---------------------*/
+
+const startBannerShow = () => {
+  const banner = document.querySelector(".banner");
+
+  let intervalBanner = 1;
+  setInterval(() => {
+    intervalBanner++;
+    banner.style.backgroundImage = `url(/images/banner${intervalBanner}.webp)`;
+    intervalBanner == 3 ? (intervalBanner = 0) : "";
+  }, 3000);
+};
+
+startBannerShow();
