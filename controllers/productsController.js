f1e7d8bae9398f710ef5b16e94ef08b4f2a0b509
Facundo Aquino <facun90@live.com>
2021-02-21 21:49:57 -0300

    catched errors on products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,148 +1,181 @@
-const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
 
 /*---------------------- database required ---------------------*/
 
-const db = require("../db/models");
+const db = require('../db/models')
 
 /*---------------------- validations ---------------------*/
 
-const { validationResult } = require("express-validator");
+const { validationResult } = require('express-validator')
 
 const controller = {
-  // Root - Show all products
-  index: async (req, res) => {
-    const startPage = Number(req.query.start) || 0;
-    const { count, rows: products } = await db.Product.findAndCountAll({
-      include: { association: "brand" },
-      offset: startPage,
-      limit: 10,
-    });
-    const totalRound = Math.floor((count + 10) / 10);
-    const page = startPage / 10 + 1;
-    res.locals.referencePage = { totalRound, page };
-    if (totalRound == page) {
-      res.locals.pagination = { continue: false, count };
-    } else {
-      res.locals.pagination = { continue: true, start: startPage + 10, count };
-    }
-    products.forEach((product) => (product.price = toThousand(product.price)));
-    res.locals.products = products;
-    res.render("products");
-  },
+	// Root - Show all products
+	index: async (req, res) => {
+		const startPage = Number(req.query.start) || 0
+		try {
+			const { count, rows: products } = await db.Product.findAndCountAll({
+				include: { association: 'brand' },
+				offset: startPage,
+				limit: 10,
+			})
+			const totalRound = Math.floor((count + 10) / 10)
+			const page = startPage / 10 + 1
+			res.locals.referencePage = { totalRound, page }
+			if (totalRound == page) {
+				res.locals.pagination = { continue: false, count }
+			} else {
+				res.locals.pagination = { continue: true, start: startPage + 10, count }
+			}
+			products.forEach((product) => (product.price = toThousand(product.price)))
+			res.locals.products = products
+			res.render('products')
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
 
-  // Detail - Detail from one product
-  show: async (req, res) => {
-    const id = req.params.productId;
+	// Detail - Detail from one product
+	show: async (req, res) => {
+		const id = req.params.productId
 
-    const product = await db.Product.findByPk(id);
+		try {
+			const product = await db.Product.findByPk(id)
 
-    product.price = toThousand(product.price);
-    res.locals.product = product;
+			product.price = toThousand(product.price)
+			res.locals.product = product
 
-    res.render("detail");
-  },
+			res.render('detail')
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
 
-  // Create - Form to create
-  create: async (req, res) => {
-    const categories = await db.Categories.findAll();
-    const brands = await db.Brands.findAll();
-    res.locals.brands = brands;
-    res.locals.categories = categories;
-    res.render("product-create-form", { errors: {}, body: {} });
-  },
+	// Create - Form to create
+	create: async (req, res) => {
+		try {
+			const categories = await db.Categories.findAll()
+			const brands = await db.Brands.findAll()
+			res.locals.brands = brands
+			res.locals.categories = categories
+			res.render('product-create-form', { errors: {}, body: {} })
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
 
-  // Create -  Method to store
-  store: async (req, res) => {
-    const errors = validationResult(req);
+	// Create -  Method to store
+	store: async (req, res) => {
+		const errors = validationResult(req)
+		try {
+			if (!errors.isEmpty() || !req.files[0]) {
+				res.locals.errors = errors.mapped()
+				res.locals.body = req.body
+				res.locals.brands = await db.Brands.findAll()
+				res.locals.categories = await db.Categories.findAll()
+				if (!req.files[0]) {
+					res.locals.errors.image = 'Ingresa una foto de tu producto!'
+				}
+				return res.render('product-create-form')
+			}
 
-    if (!errors.isEmpty() || !req.files[0]) {
-      res.locals.errors = errors.mapped();
-      res.locals.body = req.body;
-      res.locals.brands = await db.Brands.findAll();
-      res.locals.categories = await db.Categories.findAll();
-      if (!req.files[0]) {
-        res.locals.errors.image = "Ingresa una foto de tu producto!";
-      }
-      return res.render("product-create-form");
-    }
+			const { title, description, brand, category, price } = req.body
 
-    const { title, description, brand, category, price } = req.body;
+			const producModel = {
+				title,
+				brand_id: brand,
+				category_id: category,
+				price,
+				description,
+				photo: `/images/products/${req.files[0].originalname}`,
+				stock: 100,
+			}
 
-    const producModel = {
-      title,
-      brand_id: brand,
-      category_id: category,
-      price,
-      description,
-      photo: `/images/products/${req.files[0].originalname}`,
-      stock: 100,
-    };
+			const productCreated = await db.Product.create(producModel)
+			res.redirect(`/products/${productCreated.id}`)
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
 
-    const productCreated = await db.Product.create(producModel);
-    res.redirect(`/products/${productCreated.id}`);
-  },
+	// Update - Form to edit
+	edit: async (req, res) => {
+		const id = req.params.productId
 
-  // Update - Form to edit
-  edit: async (req, res) => {
-    const id = req.params.productId;
+		try {
+			const product = await db.Product.findByPk(id)
 
-    const product = await db.Product.findByPk(id);
+			res.locals.brands = await db.Brands.findAll()
+			res.locals.category = await db.Categories.findByPk(product.category_id)
+			res.locals.product = product
 
-    res.locals.brands = await db.Brands.findAll();
-    res.locals.category = await db.Categories.findByPk(product.category_id);
-    res.locals.product = product;
+			res.render('product-edit-form', { errors: {} })
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
+	// Update - Method to update
+	update: async (req, res) => {
+		const errors = validationResult(req)
+		const id = req.params.productId
 
-    res.render("product-edit-form", { errors: {} });
-  },
-  // Update - Method to update
-  update: async (req, res) => {
-    const errors = validationResult(req);
-    const id = req.params.productId;
+		try {
+			const product = await db.Product.findByPk(id)
 
-    const product = await db.Product.findByPk(id);
+			if (!errors.isEmpty()) {
+				res.locals.errors = errors.mapped()
+				res.locals.brands = await db.Brands.findAll()
+				res.locals.category = await db.Categories.findByPk(product.category_id)
+				res.locals.product = { ...req.body, id: id }
 
-    if (!errors.isEmpty()) {
-      res.locals.errors = errors.mapped();
-      res.locals.brands = await db.Brands.findAll();
-      res.locals.category = await db.Categories.findByPk(product.category_id);
-      res.locals.product = { ...req.body, id: id };
+				return res.render('product-edit-form')
+			}
 
-      return res.render("product-edit-form");
-    }
+			const { title, description, brand, price } = req.body
 
-    const { title, description, brand, price } = req.body;
+			const producModel = {
+				title,
+				brand_id: brand,
+				price,
+				description,
+				stock: 100,
+			}
+			if (req.files[0]) {
+				await db.Product.update(
+					{
+						...producModel,
+						photo: `/images/products/${req.files[0].originalname}`,
+					},
+					{ where: { id: id } }
+				)
+			} else {
+				await db.Product.update(producModel, { where: { id: id } })
+			}
 
-    const producModel = {
-      title,
-      brand_id: brand,
-      price,
-      description,
-      stock: 100,
-    };
-    if (req.files[0]) {
-      await db.Product.update(
-        {
-          ...producModel,
-          photo: `/images/products/${req.files[0].originalname}`,
-        },
-        { where: { id: id } }
-      );
-    } else {
-      await db.Product.update(producModel, { where: { id: id } });
-    }
+			res.redirect(`/products/${id}`)
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
 
-    res.redirect(`/products/${id}`);
-  },
+	// Delete - Delete one product from DB
+	destroy: async (req, res) => {
+		const id = req.params.productId
 
-  // Delete - Delete one product from DB
-  destroy: async (req, res) => {
-    const id = req.params.productId;
+		try {
+			await db.Product.destroy({ where: { id } })
 
-    await db.Product.destroy({ where: { id } });
+			res.redirect('/products')
+		} catch (error) {
+			console.log(error)
+			res.render('error-page')
+		}
+	},
+}
 
-    res.redirect("/products");
-  },
-  
-};
-
-module.exports = controller;
+module.exports = controller
